a0652808df96ee1be616d00ddf2915c59edb0b49
Lim Jun Bin <112837398+JunBin05@users.noreply.github.com>
2026-04-13 09:34:48 +0800

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,26 @@
+class MinStack {
+    List <Integer> stack;
+    int min;
+    public MinStack() {
+        stack=new ArrayList<>();
+    }
+    
+    public void push(int val) {
+        stack.add(val);
+        if (val<min){
+            min=val;
+        }
+    }
+    
+    public void pop() {
+        stack.remove(stack.size()-1);
+    }
+    
+    public int top() {
+        return stack.get(stack.size()-1);
+    }
+    
+    public int getMin() {
+        return Collections.min(stack);
+    }
+}
