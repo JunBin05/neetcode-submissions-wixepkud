c5f6b789685d14b36d5a3714b9a42ff6b488bb37
Lim Jun Bin <112837398+JunBin05@users.noreply.github.com>
2026-04-13 09:13:28 +0800

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque <Character> stack = new ArrayDeque<>();
+        HashMap <Character,Character> map = new HashMap<>();
+        map.put(']','[');
+        map.put('}','{');
+        map.put(')','(');
+        for (int i=0; i<s.length();i++){
+            char op = s.charAt(i);
+            if (map.containsValue(op)){
+                stack.push(op);
+            }else{
+                if (stack.isEmpty()){
+                    return false;
+                }
+                if (map.get(op)==stack.peek()){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if (stack.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
