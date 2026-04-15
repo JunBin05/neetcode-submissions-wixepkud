5e315932bbae7a51a543cfe482d8408ef06a8bc7
Lim Jun Bin <112837398+JunBin05@users.noreply.github.com>
2026-04-15 19:35:18 +0800

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Map <Integer, Double> moves = new TreeMap<>(Collections.reverseOrder());
+        Deque <Double> stack = new ArrayDeque<>();
+        double max=Double.MIN_VALUE;
+        int fleet=0;
+        for (int i=0; i<position.length; i++){
+            moves.put(position[i],(double)(target-position[i])/speed[i]);
+        }
+        for (double move: moves.values() ){
+            if (move>max){
+                max=move;
+                fleet++;
+            }
+        }
+        return fleet;
+    }
+}
